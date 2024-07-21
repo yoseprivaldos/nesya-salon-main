09ba034f0db90cdeb670d818abe626f7a9ae68c9
Yosep R Silaban <yoseprivaldo07@gmail.com>
2024-07-22 04:16:15 +0700

    Update build files and path configuration tolonglah terakhir ini

--- server/app.js
+++ server/app.js
@@ -65,10 +65,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
-});
-
 //ERROR HANDLING
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
